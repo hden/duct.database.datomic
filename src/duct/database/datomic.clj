98a338cd3fda4901a0ec874f3ae3164b36761238
iku000888 <kanumaiku@gmail.com>
2022-08-25 00:20:23 -0700

    Support async client/connection

--- src/duct/database/datomic.clj
+++ src/duct/database/datomic.clj
@@ -1,7 +1,10 @@
 (ns duct.database.datomic
-  (:require [cognitect.anomalies :as anomalies]
+  (:require [clojure.core.async :as a]
+            [cognitect.anomalies :as anomalies]
             [datomic.client.api :as datomic]
+            [datomic.client.api.async :as async]
             [datomic.client.api.protocols :as client-protocols]
+            [datomic.client.impl.shared.protocols :as shared-protocols]
             [diehard.core :as diehard]
             [integrant.core :as ig]))
 
@@ -20,7 +23,7 @@
    :max-retries   5
    :retry-if      retry?})
 
-(defrecord Boundary [client connection])
+(defrecord Boundary [client connection async-client async-connection])
 
 (defn- connect-ensure-database
   "Ensure that a database named db-name exists. Returns a connection."
@@ -33,12 +36,31 @@
   (diehard/with-retry retry-policy
     (datomic/connect client {:db-name db-name})))
 
+(defn- async-connect-ensure-database
+  "Ensure that a database named db-name exists. Returns a connection."
+  [{:keys [client db-name retry-policy]}]
+  {:pre [(and (satisfies? shared-protocols/Client client)
+              (string? db-name))]}
+  (when-not (contains? (into #{} (a/<!! (async/list-databases client {})))
+                       db-name)
+    (a/<!! (async/create-database client {:db-name db-name})))
+  (diehard/with-retry retry-policy
+    (a/<!! (async/connect client {:db-name db-name}))))
+
 (defmethod ig/init-key :duct.database/datomic
-  [_ {:keys [database retry-policy]
+  [_ {:keys [async? database retry-policy]
       :or {retry-policy default-retry-policy}
       :as options}]
   (let [client (datomic/client (dissoc options :database))
         connection (when database (connect-ensure-database {:client client
-                                                            :database database
+                                                            :db-name database
                                                             :retry-policy retry-policy}))]
-    (->Boundary client connection)))
+    (cond-> {:client client
+             :connection connection}
+      async? (merge (let [async-client (async/client (dissoc options :database))
+                          async-connection (when database (async-connect-ensure-database {:client async-client
+                                                                                          :db-name database
+                                                                                          :retry-policy retry-policy}))]
+                      {:async-client async-client
+                       :async-connection async-connection}))
+      :always map->Boundary)))
